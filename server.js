b57b809d55377a1fdf9a5ebf50301c1d52aabeb7
Dawid <daw.parc@gmail.com>
2023-04-24 19:53:32 +0200

    Preparing server for Replit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 //import routes
 const testimonialRoutes = require('./routes/testimonials.routes.js');
@@ -11,10 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonialRoutes);
 app.use('/api',concertsRoutes);
 app.use('/api',seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send(
@@ -22,6 +28,6 @@ app.use((req, res) => {
   );
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
